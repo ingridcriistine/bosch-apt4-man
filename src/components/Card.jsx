ff062662287585f3fbec5f67303c78eefd46c678
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-06 14:41:15 -0300

    features de produtos prontas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,14 @@
+import style from './Card.module.css'
+
 /* eslint-disable react/prop-types */
-export const Card = (props) => {
+export const Card = ({name, desc, value, image, status}) => {
   return(
-      <div>
-          <h1>{props.name}</h1>
-          <h2>{props.desc}</h2>
-          <p>{props.value}</p>
-          <img src={props.image} alt={props.name} width={150} height={"auto"}/>
+      <div className={style.boxCard}>
+          <h1>{name}</h1>
+          <h2>{desc}</h2>
+          <p>{value}</p>
+          <img src={image} alt={name} width={150} height={"auto"}/>
+          <div className={status ? style.green : style.red}></div>
       </div>
   )
 }
