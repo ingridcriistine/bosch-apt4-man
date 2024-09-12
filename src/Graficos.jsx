5730fc3a8335c34ddba00926d022ef0edee2f4ce
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-12 14:25:45 -0300

    features de graficos prontas

--- src/Graficos.jsx
+++ src/Graficos.jsx
@@ -1,50 +1,33 @@
 import style from './App.module.css'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, Legend, Tooltip, YAxis } from 'recharts';
 
 function Graficos() {
   const data = [
     {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
+      name: 'Ingrid',
+      uv: 428,
+      pv: 19
     },
     {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
+      name: 'Mariana',
+      uv: 167,
+      pv: 18
     },
     {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
+      name: 'Milena',
+      uv: 145,
+      pv: 18
     },
     {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
+      name: 'Helena',
+      uv: 325,
+      pv: 18
     },
     {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
+      name: 'Juliana',
+      uv: 167,
+      pv: 21
+    }
   ];
 
   return (
@@ -57,14 +40,31 @@ function Graficos() {
     </div>
     <div  className={style.wrapPage}>
       <h1>Exercícios de manutenção</h1>
-        <>
-          <LineChart width={600} height={300} data={data}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="name" />
-            <YAxis />
-          </LineChart>
-        </>
+        <div className={style.graficos}>
+          <div className={style.boxGrafico}>
+            <h4>Gráfico de idades</h4>
+            <LineChart width={500} height={300} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+          </div>
+
+          <div className={style.boxGrafico}>
+            <h4>Quantidade de commits</h4>
+            <LineChart width={500} height={300} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </div>
+        </div>
     </div>
     </>
   )
